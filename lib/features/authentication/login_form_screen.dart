6d91e7212ae1b837fb7f44ac3ecfdeab49428e32
lee-jaemmin <dlwoals8546@gmail.com>
2025-07-20 14:44:10 +0900

    loginForm 추가 설명

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tiktokclone/constants/gaps.dart';
 import 'package:tiktokclone/constants/sizes.dart';
+import 'package:tiktokclone/features/authentication/interest_screen.dart';
 import 'package:tiktokclone/features/authentication/widgets/form_button.dart';
 
 class LoginFormScreen extends StatefulWidget {
@@ -13,22 +14,28 @@ class LoginFormScreen extends StatefulWidget {
 class _LoginFormScreenState extends State<LoginFormScreen> {
   // Formkey로 유효성 검사, Form의 state접근 가능. unique key임.
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  Map<String, String> formData = {};
 
   void _onSubmitTap() {
     if (_formKey.currentState != null) {
+      // <- null: Form객체가 있나 없나 확인.
       if (_formKey.currentState!.validate()) {
-        // validate(): 각 Textformfield의 validator 확인
+        // validate(): 각 Textformfield의 'validator' 확인
         // if 모든 게 null: true, 하나라도 not null: false
         _formKey.currentState!.save();
-        //save: 모든 입력 텍스트에 대해 onSaved() 콜백 함수 실행
+        //save(): 모든 입력 텍스트에 대해 onSaved 콜백 함수 실행
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => InterestScreen(),
+          ),
+        );
       }
     }
 
     /* 2. ? 써서 null이면 아무것도 하지 않게 함.
     _formKey.currentState?.validate(); */
   }
-
-  Map<String, String> formData = {};
 
   @override
   Widget build(BuildContext context) {
@@ -58,6 +65,20 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                       ),
                     ),
                   ),
+                  validator: (value) {
+                    // return null: 정상 , not null 비정상(오류)
+                    // value 는 String
+                    if (value != null && value.isEmpty) {
+                      // 클릭도 안함, 클릭만 함 (입력 x)
+                      return "Please write your email";
+                    }
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    if (newValue != null) {
+                      formData['Email'] = newValue;
+                    }
+                  },
                 ),
                 Gaps.v16,
                 TextFormField(
@@ -75,6 +96,10 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                     ),
                   ),
                   validator: (value) {
+                    if (value != null && value.isEmpty) {
+                      // 클릭도 안함, 클릭만 함 (입력 x)
+                      return "Please Valid Password";
+                    }
                     return null;
                   },
                   onSaved: (newValue) {
