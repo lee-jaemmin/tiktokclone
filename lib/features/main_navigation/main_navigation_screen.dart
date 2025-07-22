1f3a9795d221ddafb362967b65778eb5522f9f5a
lee-jaemmin <dlwoals8546@gmail.com>
2025-07-22 16:24:03 +0900

    custom_navbar

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktokclone/constants/gaps.dart';
+import 'package:tiktokclone/constants/sizes.dart';
+import 'package:tiktokclone/features/main_navigation/widgets/nav_tab.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   MainNavigationScreen({super.key});
@@ -37,25 +40,46 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.shifting,
-        currentIndex: _selectedIndex,
-        onTap: _onTap,
-        selectedItemColor: Theme.of(context).primaryColor,
-        unselectedItemColor: Colors.grey.shade500,
-        items: [
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.house),
-            label: "Home",
-            tooltip: "What are you?",
+      bottomNavigationBar: BottomAppBar(
+        height: 100,
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.size12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                text: "Home",
+                isSelected: _selectedIndex == 0,
+                icon: FontAwesomeIcons.house,
+                myonTap: () => _onTap(0),
+              ),
+              NavTab(
+                text: "Discover",
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.magnifyingGlass,
+                myonTap: () => _onTap(1),
+              ),
+              NavTab(
+                text: "Inbox",
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.inbox,
+                myonTap: () => _onTap(3),
+              ),
+              NavTab(
+                text: "Profile",
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                myonTap: () => _onTap(4),
+              ),
+              /*
+              정리하자면, 함수를 콜백한다는 전제는 똑같다.
+              인자가 없으면 함수 자체를 콜백할 수  있으나 인자를 써버리면 빌드될 때 바로 실행돼서 
+              이 경우에는 익명함수에 감싸서 함수 (익명) 내부에서 함수를 실행하는 구조를 만들어버리는 것이다.
+              */
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
-            label: "Search",
-            tooltip: "What are you?",
-          ),
-        ],
+        ),
       ),
     );
   }
---
+++ lib/features/main_navigation/widgets/nav_tab.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktokclone/constants/gaps.dart';
+
+class NavTab extends StatelessWidget {
+  const NavTab({
+    super.key,
+    required this.text,
+    required this.isSelected,
+    required this.icon,
+    required this.myonTap,
+  });
+
+  final String text;
+  final bool isSelected;
+  final IconData icon;
+  final Function myonTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      // 어디를 눌러도 실행되게!
+      child: GestureDetector(
+        onTap: () => myonTap(),
+        /*
+        onTap은 정확히 void Function() 타입의 함수를 기대함
+        Function은 추상적이므로 명확한 타입이 아니고, 타입 검사에 실패할 수 있음
+        () => ... 형태의 익명 함수는 Dart가 void Function()으로 추론해줌 → 전달 가능
+        */
+
+        /*
+         익명 함수에 감쌌으므로 우리가 직접 실행해야돼서 onTap이 아닌 onTap()으로.
+         인자가 있을 때 () => onTap(1)이런식으로 했지? 이것도 ()를 붙여서 직접 실행해준 것에 해당
+        */
+        child: Container(
+          color: Colors.black,
+          child: AnimatedOpacity(
+            duration: Duration(milliseconds: 100),
+            opacity: isSelected ? 1 : 0.5,
+            child: Column(
+              children: [
+                FaIcon(
+                  icon,
+                  color: Colors.white,
+                ),
+                Gaps.v5,
+                Text(
+                  text,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
